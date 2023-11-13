fbf8b4487ab2e7bc21a744fc056ceac4635feeb9
Naman Gupta <namangupta.cse@gmail.com>
2023-11-13 05:49:48 +0530

    Update Post and Modal

--- src/components/shared/Post/index.tsx
+++ src/components/shared/Post/index.tsx
@@ -1,29 +1,54 @@
 import Image from "next/image";
 
+import styles from "./index.module.css";
+
 // types
 import { TPost } from "@/@types";
 
+//utils
+import { parseInRelativeTime } from "@/utils/date";
+
 function Post({ post }: { post: TPost }) {
   return (
-    <div>
-      <div>
-        <Image src={post.avatar} alt={post.authorName} width={40} height={40} />
-        <div>
-          <p>{post.authorName}</p>
-          <p>
-            {post.updatedAt.toString()}{" "}
-            <span>{post.createdAt !== post.updatedAt ? "Edited" : ""}</span>
-          </p>
+    <div className={styles.container}>
+      <div className={styles.postHeaderContainer}>
+        <Image
+          className={styles.authorImage}
+          src={post.avatar}
+          alt={post.authorName}
+          width={40}
+          height={40}
+        />
+        <div className={styles.authorDetailsContainer}>
+          <span className={styles.author}>{post.authorName}</span>
+          <span className={styles.subtleLabel}>
+            {parseInRelativeTime(post.updatedAt)}{" "}
+            <span>{post.createdAt !== post.updatedAt ? "• Edited" : ""}</span>
+          </span>
         </div>
-        <img alt="More options" />
+        <img src="/threedots.svg" alt="More options" />
       </div>
-      <div>
-        <div>{post.emoji}</div>
-        <div>{post.content}</div>
+      <div className={styles.postContentContainer}>
+        <div className={styles.emojiIconContainer}>
+          <span className={styles.emoji}>{post.emoji}</span>
+        </div>
+        <div className={styles.content}>{post.content}</div>
       </div>
-      <div>
-        <img />
-        <span>{post.totalComments.toString()} comments</span>
+      <div className={styles.commentsContainer}>
+        <img
+          className={styles.commentsIcon}
+          src="/comments.svg"
+          alt="Comments Icon"
+        />
+        <span
+          role="button"
+          tabIndex={0}
+          aria-label={`${post.totalComments.toString()} comments`}
+          className={styles.subtleLabel}
+        >
+          {/* Clicking on comments can take the post id and make a GET request. This will allow to fetch comments on deemand */}
+          {post.totalComments.toString()} comments
+        </span>
       </div>
     </div>
   );
---
+++ src/components/ui/Modal/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+import styles from "./index.module.css";
+import React from "react";
+import { useState, useCallback, useMemo } from "react";
+const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = useCallback(() => setIsOpen(true), []);
+  const closeModal = useCallback(() => setIsOpen(false), []);
+
+  const ModalComponent = useMemo(
+    () =>
+      ({ children }: { children: React.ReactNode }) =>
+        (
+          <div className={styles.modalBackdrop} onClick={closeModal}>
+            <div
+              className={styles.modalContent}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <button className={styles.closeButton} onClick={closeModal}>
+                <img src="/close.svg" alt="Close" />
+              </button>
+              {children}
+            </div>
+          </div>
+        ),
+    [closeModal]
+  );
+
+  return { isOpen, openModal, closeModal, ModalComponent };
+};
+
+export default useModal;
